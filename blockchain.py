a15b8c44da9858e7765fe42976928273d64805b8
Eltaf-azizi <iltfhuss@gmail.com>
2025-04-29 11:03:31 +0500

    worked on new block

--- blockchain.py
+++ blockchain.py
@@ -1,10 +1,33 @@
+import hashlib
+import json
+from time import time
+
+
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
 
-    def new_block(self):
-        pass
+    def new_block(self, proof, previous_hash=None):
+        """
+        Create a new block in Blockchain
+        :param proof: <int> The proof given by the Proof of work algorithm
+        :param previous_hash: (optional) <str> The hash of the previous block
+        :return: <dict> New Block
+        """
+
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            previous_hash: previous_hash or hash(self.chain[-1]),
+        }
+
+        self.current_transactions = []
+
+        self.chain.append(block)
+        return block
 
 
     def new_transaction(self, sender, recipient, amount):
