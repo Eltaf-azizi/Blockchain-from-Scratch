ed90dddc4617bf7f51281d967a0860818b3b89fe
Eltaf-azizi <iltfhuss@gmail.com>
2025-05-01 12:40:55 +0500

    working on proof_of_work

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,7 @@
 import hashlib
 import json
 from time import time
+from uuid import uuid4
 
 
 class Blockchain(object):
@@ -57,10 +58,34 @@ class Blockchain(object):
     @staticmethod
     def hash(block):
         """
-        Creates a SHA-256 hash of a Block
+        Creates SHA-256 hash of a Block
         :param block: <dict> Block
         :return: <str>
         """
         block_string = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(block_string).hexdigest()
 
+
+
+    def proof_of_work(self, last_proof):
+        """
+        Simple proof or Work Algoritm:
+        - Find a number p; such that hash(pp') contains leading 4 zeroes, where p
+        - p is the previous proof, and p' is the new proof
+        :param last_proof: <int>
+        :return: <int>
+        """
+
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
+
+
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        """
+        
+        """
