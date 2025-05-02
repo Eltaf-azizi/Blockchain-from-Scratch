37d5cfb20de9f4d003d52e655190f66c1b467927
Eltaf-azizi <iltfhuss@gmail.com>
2025-05-02 11:51:20 +0500

    working on the setup

--- blockchain.py
+++ blockchain.py
@@ -3,17 +3,35 @@ import json
 from textwrap import dedent
 from time import time
 from uuid import uuid4
-from flask import Flask
+from flask import Flask, jsonify
 
 
 class Blockchain(object):
-
     # Instantiate our Node
     app = Flask(__name__)
 
     # Generate a globally unique address for this node
     node_identifier = str(uuid4()).replace('-', '')
 
+
+    # Instantiate the Blockchain
+    blockchain = Blockchain()
+
+    @app.route('/mine', methods=['GET'])
+    def mine(self):
+        return "We'll mine a new block"
+
+    @app.route('/transactions/new', methods=['POST'])
+    def new_transaction():
+        return "We'll add a new transaction"
+
+    @app.route('/chain', methods=['GET'])
+    def full_chain(blockchain=None):
+        response = {
+            'chain': blockchain.chain,
+            'length': len(blockchain.chain),
+        }
+        return jsonify(response), 200
 
 
     def __init__(self):
