83c7fda7d39e5c80a147a41339c311bdf3e3dd10
Eltaf-azizi <iltfhuss@gmail.com>
2025-05-07 12:20:37 +0500

    working to grab and varify the chains from all the nodel in our networks

--- blockchain.py
+++ blockchain.py
@@ -53,6 +53,29 @@ class Blockchain(object):
         """
 
         neighbours = self.nodes
+        new_chain = None
+
+
+        # We're only lokking for chains longer than ours
+        max_length = len(self.chain)
+
+
+
+        # Grab and verify the chains from all the nodes in our network
+        for node in neighbours:
+            response = requests.get(f'http://{node}/chain')
+
+
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+
+
+                # Check if the length is longer and the chain is valid
+                if length > max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+                
 
 
 
