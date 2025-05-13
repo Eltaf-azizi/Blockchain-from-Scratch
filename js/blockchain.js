f3f77e0848310df566d5d3aef1578162142cda5a
Eltaf-azizi <iltfhuss@gmail.com>
2025-05-13 12:06:18 +0500

    working on the new block

--- js/blockchain.js
+++ js/blockchain.js
@@ -15,3 +15,22 @@ class Blockchain {
     addPeer(host) {
         this.peers.add(host);
     }
+    
+    /**
+     * Adds a node to our peer table
+     */
+    getPeers() {
+        return Array.from(this.peers);
+    }
+
+    /**
+     * Creates a new block containing any outstanding transactions
+     */
+    newBlock(previousHash, nonce = null) {
+        let block = {
+            index: this.chain.length,
+            timestamp: new Date().toISOString(),
+            transactions: this.pendingTransactions,
+            previousHash,
+            nonce
+        };
