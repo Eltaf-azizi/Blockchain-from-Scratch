111d110e26db566a9b1fc603170d1bc1dd9d8f40
Eltaf-azizi <iltfhuss@gmail.com>
2025-05-14 11:36:40 +0500

    working on the switch function

--- js/index.js
+++ js/index.js
@@ -11,3 +11,28 @@ module.exports = async (request, response) => {
     blockchain.addPeer(request.headers.host);
 
     let output;
+    
+
+    switch (route) {
+        case "/new_block":
+            output = blockchain.newBlock();
+            break;
+
+        case "/last_block":
+            output = blockchain.lastBlock();
+            break;
+
+        case "/get_peers":
+            output = blockchain.getPeers();
+            break;
+
+        case "/submit_transaction":
+            output = blockchain.addTransaction(transaction);
+            break;
+
+        default:
+            output = blockchain.lastBlock();
+
+    }
+    send(response, 200, output);
+};
