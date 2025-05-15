65db3e6f567cdfed82375501bbe2366b65d2b074
Eltaf-azizi <iltfhuss@gmail.com>
2025-05-15 23:37:54 +0500

    working on the testRegisternodes

--- tests/test_blockchain.py
+++ tests/test_blockchain.py
@@ -19,3 +19,28 @@ class BlockchainTestCase(TestCase):
             recipient=recipient,
             amount=amount
         )
+
+class TestRegisterNodes(BlockchainTestCase):
+
+    def test_valid_nodes(self):
+        blockchain = Blockchain()
+
+        blockchain.register_node('http://192.168.0.1:5000')
+
+        self.assertIn('192.168.0.1:5000', blockchain.nodes)
+
+    def test_malformed_nodes(self):
+        blockchain = Blockchain()
+
+        blockchain.register_node('http//192.168.0.1:5000')
+
+        self.assertNotIn('192.168.0.1:5000', blockchain.nodes)
+
+    def test_idempotency(self):
+        blockchain = Blockchain()
+
+        blockchain.register_node('http://192.168.0.1:5000')
+        blockchain.register_node('http://192.168.0.1:5000')
+
+        assert len(blockchain.nodes) == 1
+
