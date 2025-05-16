6ba7e4f608eddf71ab54071c8752153f7c91be9f
Eltaf-azizi <iltfhuss@gmail.com>
2025-05-16 11:11:51 +0500

    working on the test block and transactions

--- tests/test_blockchain.py
+++ tests/test_blockchain.py
@@ -44,3 +44,29 @@ class TestRegisterNodes(BlockchainTestCase):
 
         assert len(blockchain.nodes) == 1
 
+
+
+
+class TestBlocksAndTransactions(BlockchainTestCase):
+
+    def test_block_creation(self):
+        self.create_block()
+
+        latest_block = self.blockchain.last_block
+
+        # The genesis block is create at initialization, so the length should be 2
+        assert len(self.blockchain.chain) == 2
+        assert latest_block['index'] == 2
+        assert latest_block['timestamp'] is not None
+        assert latest_block['proof'] == 123
+        assert latest_block['previous_hash'] == 'abc'
+
+    def test_create_transaction(self):
+        self.create_transaction()
+
+        transaction = self.blockchain.current_transactions[-1]
+
+        assert transaction
+        assert transaction['sender'] == 'a'
+        assert transaction['recipient'] == 'b'
+        assert transaction['amount'] == 1
