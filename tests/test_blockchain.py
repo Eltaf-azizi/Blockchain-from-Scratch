adb3f2b01407166d9de2f9e7809b538d5572693e
Eltaf-azizi <iltfhuss@gmail.com>
2025-05-16 15:52:10 +0500

    working on the test hashing and proofs

--- tests/test_blockchain.py
+++ tests/test_blockchain.py
@@ -92,3 +92,18 @@ class TestBlocksAndTransactions(BlockchainTestCase):
 
         assert len(self.blockchain.chain) == 2
         assert created_block is self.blockchain.chain[-1]
+
+
+
+
+class TestHashingAndProofs(BlockchainTestCase):
+
+    def test_hash_is_correct(self):
+        self.create_block()
+
+        new_block = self.blockchain.last_block
+        new_block_json = json.dumps(self.blockchain.last_block, sort_keys=True).encode()
+        new_hash = hashlib.sha256(new_block_json).hexdigest()
+
+        assert len(new_hash) == 64
+        assert new_hash == self.blockchain.hash(new_block)
