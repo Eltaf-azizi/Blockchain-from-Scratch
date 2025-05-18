6559a97ddb893cef5da101860220d20dda10d327
Eltaf-azizi <iltfhuss@gmail.com>
2025-05-18 16:33:35 +0500

    working on the private functionality

--- csharp/BlockChain/BlockChain.cs
+++ csharp/BlockChain/BlockChain.cs
@@ -18,3 +18,34 @@ namespace BlockChainDemo
         private Block _lastBlock => _chain.Last();
 
         public string NodeId { get; private set; }
+
+
+        
+        //ctor
+        public BlockChain()
+        {
+            NodeId = Guid.NewGuid().ToString().Replace("-", "");
+            CreateNewBlock(proof: 100, previousHash: "1"); //genesis block
+        }
+
+        //private functionality
+        private void RegisterNode(string address)
+        {
+            _nodes.Add(new Node { Address = new Uri(address) });
+        }
+
+        private bool IsValidChain(List<Block> chain)
+        {
+            Block block = null;
+            Block lastBlock = chain.First();
+            int currentIndex = 1;
+            while (currentIndex < chain.Count)
+            {
+                block = chain.ElementAt(currentIndex);
+                Debug.WriteLine($"{lastBlock}");
+                Debug.WriteLine($"{block}");
+                Debug.WriteLine("----------------------------");
+
+                //Check that the hash of the block is correct
+                if (block.PreviousHash != GetHash(lastBlock))
+                    return false;
