14706bbf70b66588d9fee56fde8862a758328793
Eltaf-azizi <iltfhuss@gmail.com>
2025-05-18 16:16:40 +0500

    working on the block chain in c sharp

---
+++ csharp/BlockChain/BlockChain.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace BlockChainDemo
+{
+    public class BlockChain
+    {
+        private List<Transaction> _currentTransactions = new List<Transaction>();
+        private List<Block> _chain = new List<Block>();
+        private List<Node> _nodes = new List<Node>();
+        private Block _lastBlock => _chain.Last();
+
+        public string NodeId { get; private set; }
