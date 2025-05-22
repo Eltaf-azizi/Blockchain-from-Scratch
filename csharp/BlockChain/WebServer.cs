788693f99a1f8f2da63ad638148147b5bb410bcf
Eltaf-azizi <iltfhuss@gmail.com>
2025-05-22 10:56:50 +0500

    start working on the web server

---
+++ csharp/BlockChain/WebServer.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System.Configuration;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+
+namespace BlockChainDemo
+{
+    public class WebServer
+    {
+        public WebServer(BlockChain chain)
+        {
+            var settings = ConfigurationManager.AppSettings;
+            string host = settings["host"]?.Length > 1 ? settings["host"] : "localhost";
+            string port = settings["port"]?.Length > 1 ? settings["port"] : "12345";
+
+            var server = new TinyWebServer.WebServer(request =>
+                {
+                    string path = request.Url.PathAndQuery.ToLower();
+                    string query = "";
+                    string json = "";
+                    if (path.Contains("?"))
+                    {
+                        string[] parts = path.Split('?');
+                        path = parts[0];
+                        query = parts[1];
+                    }
